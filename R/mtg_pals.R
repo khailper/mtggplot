db080cbfb9218146697d65ffd20b6ad696f1e34d
Karl_Hailperin <khailperin@data-blitz.com>
2019-06-30 08:48:49 -0500

    basic framework

---
+++ R/mtg_pals.R
@@ -0,0 +1,28 @@
+#' this code is heavily based on
+#' https://drsimonj.svbtle.com/creating-corporate-colour-palettes-for-ggplot2
+
+mtg_colors <- c(
+  `White` = "#f9faf4",
+  `Blue` = "#0e68ab",
+  `Black` = "#150b00",
+  `Red` = "#d3202a",
+  `Green` = "#00733e",
+  `Multicolored` = "#e1cd92",
+  `Colorless` = "#c6c5c5"
+)
+
+#' Function to extract MtG colors as hex codes
+#'
+#' @param ... Character names of mtg_colors. Options are "White", "Blue",
+#' "Black",  "Red", "Green", "Multicolored", and "Colorless".
+#'
+#'
+#' @export
+mtg_cols <- function(...) {
+  cols <- c(...)
+
+  if (is.null(cols))
+    return (mtg_colors)
+
+  mtg_colors[cols]
+}
